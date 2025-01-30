670a2ab0fc62d9a635c6e3af03c45222b7569381
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-01-30 21:33:47 +0600

    update

--- ES6/filter.js
+++ ES6/filter.js
@@ -10,5 +10,31 @@ const result=numbers.filter(n=>n>82);
 // find
 
 const findResult=numbers.find(n=>n>70);
-console.log(findResult);
+// console.log(findResult);
+
+
+
+
+// optional use for practice  /////////////
+
+
+const nums=[1,4,7,8,11,12];
+
+// const numbs=nums.filter(nums=>nums%2 === 0);
+
+let reserve=[];
+let reserveOdd=[];
+for(const num of nums){
+    if(num%2 === 0){
+        reserve.push(num);
+    }
+    else if(num%2 !== 0){
+        reserveOdd.push(num);
+    }
+}
+console.log(reserve);
+console.log(reserveOdd);
+
+
+// console.log(numbs);
 
