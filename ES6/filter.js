c3df56e557c63b95b62c39cca14270f3e574015f
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-01-30 11:27:48 +0600

    filter done

---
+++ ES6/filter.js
@@ -0,0 +1,14 @@
+// Filter 
+
+const numbers= [30,40,42,55,60,75,81,86];
+
+const result=numbers.filter(n=>n>82);
+// console.log(result)
+
+
+
+// find
+
+const findResult=numbers.find(n=>n>70);
+console.log(findResult);
+
