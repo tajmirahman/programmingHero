a0d22778cb9d0c02a15989d20b920c562a412195
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-01-31 21:43:39 +0600

    closer

--- practice.js
+++ practice.js
@@ -1,24 +1,8 @@
-function claculateTicketPrice(age, student = false) {
-
-    const ticketPrice = 800;
-
-    if (age < 10) {
-        return 0;
-    } else {
-        if (student == true) {
-            return ticketPrice * .5;
-        } else {
-            if (age >= 60) {
-                return ticketPrice * .85;
-            } else {
-                return ticketPrice;
-            }
-        }
-    }
-
-}
-
-console.log(claculateTicketPrice(11));
-console.log(claculateTicketPrice(20, true));
-console.log(claculateTicketPrice(59));
-console.log(claculateTicketPrice(30));
+document.getElementById('btn-send').addEventListener('click',()=>{
+    const inputText=document.getElementById('input-text');
+    const text=inputText.value;
+    const commentContainer=document.getElementById('comment-container');
+    const p=document.createElement('p');
+    commentContainer.appendChild(p);
+    p.innerText=text;
+})
